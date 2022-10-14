865cd12440f0ee9b5891ad28404640791489292e
BacoN1004 <101983525+BacoN1004@users.noreply.github.com>
2022-10-14 19:24:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var socials = function(){
+    window.location = "https://fius.it/socials";
+}
+var development = function(){
+    window.location = "https://fius.it/development";
+}
+var entertainment = function(){
+    window.location = "https://fius.it/entertainment";
+}
+var statuss = function(){
+    window.location = "https://status.fius.it/";
+}
+var blogs = function(){
+    window.location = "https://fius.it/blog";
+}
+var BacoNHouse = function(){
+    window.location = "https://bacon1004.github.io/BacoN-House/";
+}
